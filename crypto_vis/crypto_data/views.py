2aa0f03b5504220b73dc42cd1a82e96fb8129540
Daniel <danielcoellovargas@gmail.com>
2025-07-14 22:58:33 -0500

    Refactor templates and add 7-day price trend chart
    Refactored home and about templates to use base.html for consistent styling and layout. Enhanced the home page with a bar chart for top 10 cryptocurrencies and a new 7-day price trend line chart, including dual y-axes for Bitcoin and other coins. Updated views.py to provide the necessary data for these charts and improved the about page contact information.

--- crypto_vis/crypto_data/views.py
+++ crypto_vis/crypto_data/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from pycoingecko import CoinGeckoAPI
-
-cg = CoinGeckoAPI()
+import datetime
 
 def home(request):
+    cg = CoinGeckoAPI()
+
     # Fetch top 100 cryptocurrencies by market cap
-    data = cg.get_coins_markets(vs_currency='usd', per_page=100, page=1)
-    # Prepare chart data (top 10 for readability)
-    labels = [coin['name'] for coin in data[:10]]
-    prices = [coin['current_price'] for coin in data[:10]]
+    top_100 = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=100, page=1)
+    
+    # Extract top 10 for charts
+    top_10 = top_100[:10]
+
+    # Bar chart data
+    bar_labels = [coin['name'] for coin in top_10]
+    bar_data = [coin['current_price'] for coin in top_10]
+
+    historical_data = {}
+    for coin in top_10:
+        coin_id = coin['id']
+        historical = cg.get_coin_market_chart_by_id(id = coin_id, vs_currency='usd', days=7)
+        prices = [price[1] for price in historical['prices']][:7]
+        historical_data[coin_id] = prices
+
+    today = datetime.date.today()
+    dates = [(today - datetime.timedelta(days=i)).strftime('%Y-%m-%d') for i in range(6, -1, -1)]
     
     context = {
-        'crypto_data': data,
-        'chart_labels': labels,
-        'chart_data': prices,
+        'crypto_data': top_100,
+        'bar_labels': bar_labels,
+        'bar_data': bar_data,
+        'historical_data': historical_data,
+        'dates': dates,
+        'top_10_ids': [coin['id'] for coin in top_10]
     }
     return render(request, 'crypto_data/home.html', context)
 
